6f5290e330297d79157d796a7433cb19d1c3d29d
benjamin-t-brown <benjamin-t-brown@users.noreply.github.com>
2021-03-05 00:47:28 +0000

    deploy: 310ea3a36cd43dd1ecee69688f3235326701cb6b

--- iframes/lib.js
+++ iframes/lib.js
@@ -1,90 +1,232 @@
-var startButtonEnabled = true;
+function LIB() {
+  const config = {
+    startButtonEnabled: true,
+    soundEnabled: true,
+    shouldShowControls: true,
+    originalScale: false,
+    gameStarted: false,
+  };
 
-var soundEnabled = true;
-function toggleSound() {
-  if (soundEnabled) {
-    console.log('Disable sound');
-    Module.ccall('disableSound');
-  } else {
-    console.log('Enabled sound');
-    Module.ccall('enableSound');
-  }
-  soundEnabled = !soundEnabled;
+  this.toggleSound = function () {
+    if (config.soundEnabled) {
+      console.log('[IFRAME] Disable sound');
+      Module.ccall('disableSound');
+    } else {
+      console.log('[IFRAME] Enabled sound');
+      Module.ccall('enableSound');
+    }
+    config.soundEnabled = !config.soundEnabled;
+  };
+  this.toggleScale = function () {
+    const board = document.getElementById('board');
+    const canvas = document.getElementById('canvas');
+    if (config.originalScale) {
+      board.className = 'board';
+      canvas.className = '';
+    } else {
+      board.className = 'board-original';
+      canvas.className = 'canvas-original';
+    }
+    config.originalScale = !config.originalScale;
+  };
+  this.toggleControls = function () {
+    if (config.shouldShowControls) {
+      this.hideControls();
+    } else {
+      this.showControls();
+    }
+    config.shouldShowControls = !config.shouldShowControls;
+  };
+  this.hideLoading = function () {
+    const loading = document.getElementById('loading');
+    if (loading) loading.style.display = 'none';
+    const game = document.getElementById('game');
+    if (game) game.style.display = 'flex';
+    const error = document.getElementById('error');
+    if (error) error.style.display = 'none';
+  };
+  this.showError = function () {
+    const loading = document.getElementById('loading');
+    if (loading) loading.style.display = 'none';
+    const game = document.getElementById('game');
+    if (game) game.style.display = 'none';
+    const error = document.getElementById('error');
+    if (error) error.style.display = 'flex';
+  };
+  this.hideControls = function () {
+    const top = document.getElementById('top-bar-controls');
+    if (top) top.style.display = 'none';
+    const controls = document.getElementById('on-screen-controls');
+    if (controls) controls.style.display = 'none';
+  };
+  this.showControls = function () {
+    const top = document.getElementById('top-bar-controls');
+    if (top) top.style.display = 'flex';
+    const controls = document.getElementById('on-screen-controls');
+    if (controls) controls.style.display = 'flex';
+  };
+  this.handleButtonDown = function (key) {
+    Module.ccall('setKeyDown', 'void', ['number'], [key]);
+  };
+  this.handleButtonUp = function (key) {
+    Module.ccall('setKeyUp', 'void', ['number'], [key]);
+  };
+  this.disableModuleControls = function () {
+    Module.ccall('setKeyStatus', 'void', ['number'], [0]);
+  };
+  this.enableModuleControls = function () {
+    Module.ccall('setKeyStatus', 'void', ['number'], [1]);
+  };
+
+  this.notifyParentFrame = function (action, payload) {
+    if (window.parent) {
+      console.log('[IFRAME] Notify parent', action);
+      window.parent.postMessage(
+        JSON.stringify({
+          action,
+          payload,
+        })
+      );
+    } else {
+      console.log('[IFRAME] No parent to notify.');
+    }
+  };
+
+  this.notifyGameReady = function () {
+    this.notifyParentFrame('GAME_READY', {});
+  };
+
+  this.notifyGameStarted = function () {
+    this.notifyParentFrame('GAME_STARTED', {});
+    config.gameStarted = true;
+  };
+
+  this.notifyGameCompleted = function (result) {
+    this.notifyParentFrame('GAME_CONCLUDED', result);
+    config.gameStarted = false;
+  };
+
+  this.notifyGameCancelled = function () {
+    this.notifyParentFrame('GAME_CANCELLED', {});
+    config.gameStarted = false;
+  };
+
+  const SOUND_PATH = '';
+  const sounds = {};
+
+  this.loadSound = async function (name, url) {
+    url = `${SOUND_PATH}${url}`;
+    return new Promise((resolve, reject) => {
+      const sound = new Audio(url);
+      sound.autoplay = false;
+      const onLoad = () => {
+        sound.oncanplay = null;
+        sound.currentTime = 99999999999;
+        const soundDuration = sound.currentTime;
+        sound.currentTime = 0;
+        sound.onended = function () {
+          sound.pause();
+          sound.currentTime = 0;
+        };
+        sounds[name] = {
+          sound,
+          audio: sound,
+          soundDuration,
+        };
+        console.log('[IFRAME] sound loaded', name, url);
+        clearTimeout(timeoutId);
+        resolve(sound);
+      };
+      sound.oncanplay = sound.onloadeddata = onLoad;
+      const timeoutId = setTimeout(() => {
+        console.error('[IFRAME] Sound load timed out:', name, url);
+        onLoad();
+      }, 3000);
+
+      sound.addEventListener('error', e => {
+        console.error('sound error', e);
+        reject(
+          '[IFRAME] Cannot load sound: name="' + name + '", url="' + url + '"'
+        );
+      });
+      sound.src = url;
+    });
+  };
+
+  this.getSound = function (soundName) {
+    const soundObj = sounds[soundName];
+    if (soundObj) {
+      const s = {
+        duration: 0,
+        ...soundObj,
+        //soundDuration merged in from soundObj
+        audio: soundObj.audio.cloneNode(),
+        soundName,
+        lastStartTimestamp: window.performance.now(),
+        isPlaying: false,
+        isPaused: false,
+      };
+
+      return s;
+    } else {
+      console.error('Could not find sound with name: ', soundName);
+      return null;
+    }
+  };
+
+  this.playSound = function (soundObj) {
+    if (!config.soundEnabled) {
+      return;
+    }
+    const { sound } = soundObj;
+    sound.play();
+    soundObj.lastStartTimestamp = window.performance.now();
+    soundObj.isPlaying = true;
+  };
+
+  this.playSoundName = function (soundName) {
+    const soundObj = this.getSound(soundName);
+    if (soundObj) {
+      this.playSound(soundObj);
+    }
+  };
+
+  this.stopSound = function (soundObj) {
+    const { sound } = soundObj;
+    sound.pause();
+    sound.currentTime = 0;
+    soundObj.isPlaying = false;
+  };
+
+  this.getConfig = function () {
+    return config;
+  };
 }
-var originalScale = false;
-function toggleScale() {
-  const board = document.getElementById('board');
-  const canvas = document.getElementById('canvas');
-  if (originalScale) {
-    board.className = 'board';
-    canvas.className = '';
-  } else {
-    board.className = 'board-original';
-    canvas.className = 'canvas-original';
-  }
-  originalScale = !originalScale;
-}
-var shouldShowControls = true;
-function toggleControls() {
-  if (shouldShowControls) {
-    hideControls();
-  } else {
-    showControls();
-  }
-  shouldShowControls = !shouldShowControls;
-}
-function hideLoading() {
-  const loading = document.getElementById('loading');
-  if (loading) loading.style.display = 'none';
-  const game = document.getElementById('game');
-  if (game) game.style.display = 'flex';
-  const error = document.getElementById('error');
-  if (error) error.style.display = 'none';
-}
-function showError() {
-  const loading = document.getElementById('loading');
-  if (loading) loading.style.display = 'none';
-  const game = document.getElementById('game');
-  if (game) game.style.display = 'none';
-  const error = document.getElementById('error');
-  if (error) error.style.display = 'flex';
-}
-function hideControls() {
-  const top = document.getElementById('top-bar-controls');
-  if (top) top.style.display = 'none';
-  const controls = document.getElementById('on-screen-controls');
-  if (controls) controls.style.display = 'none';
-}
-function showControls() {
-  const top = document.getElementById('top-bar-controls');
-  if (top) top.style.display = 'flex';
-  const controls = document.getElementById('on-screen-controls');
-  if (controls) controls.style.display = 'flex';
-}
-function handleButtonDown(key) {
-  Module.ccall('setKeyDown', 'void', ['number'], [key]);
-}
-function handleButtonUp(key) {
-  Module.ccall('setKeyUp', 'void', ['number'], [key]);
-}
+
+var Lib = (window.Lib = new LIB());
+
 var Module = {
   jsLoaded: function () {
     Module.preRun[0]();
   },
   preRun: [
     function () {
-      hideLoading();
+      Lib.hideLoading();
       clearTimeout(window.loadTimeout);
-      notifyGameReady();
     },
   ],
   postRun: [
     function () {
       var shouldMute = params.get('mute');
       if (shouldMute === 'true') {
-        soundEnabled = true;
-        toggleSound();
+        Lib.getConfig().soundEnabled = true;
+        Lib.toggleSound();
       }
+      var isArcadeCabinet = expand === 'true';
+      if (isArcadeCabinet) {
+        Lib.disableModuleControls();
+      }
+      Lib.notifyGameReady();
     },
   ],
   canvas: (function () {
@@ -96,7 +238,7 @@ var Module = {
           console.error(
             '[IFRAME] WebGL context lost. You will need to reload the page.'
           );
-          showError();
+          Lib.showError();
           e.preventDefault();
         },
         false
@@ -107,7 +249,7 @@ var Module = {
   })(),
   onAbort: function () {
     console.error('[IFRAME] Program encountered an unknown error.');
-    showError();
+    Lib.showError();
   },
   totalDependencies: 0,
   ccall: function () {},
@@ -123,60 +265,31 @@ window.addEventListener(
   true
 );
 
-function notifyParentFrame(action, payload) {
-  if (window.parent) {
-    window.parent.postMessage(
-      JSON.stringify({
-        action,
-        payload,
-      })
-    );
-  } else {
-    console.log('[IFRAME] No parent to notify', TYPE);
-  }
-}
-
-function notifyGameReady() {
-  notifyParentFrame('GAME_READY', {});
-}
-
-function notifyGameStarted() {
-  notifyParentFrame('GAME_STARTED', {});
-}
-
-function notifyGameCompleted(score) {
-  notifyParentFrame('GAME_CONCLUDED', { score });
-}
-
-function notifyGameCancelled() {
-  notifyParentFrame('GAME_CANCELLED', {});
-}
-
 window.addEventListener('message', event => {
   try {
     const data = JSON.parse(event.data);
     if (data.action === 'HIDE_CONTROLS') {
-      shouldShowControls = true;
-      toggleControls();
+      Lib.getConfig().shouldShowControls = true;
+      Lib.toggleControls();
     } else if (data.action === 'SHOW_CONTROLS') {
-      shouldShowControls = false;
-      toggleControls();
+      Lib.getConfig().shouldShowControls = false;
+      Lib.toggleControls();
     } else if (data.action === 'SCALE_ORIGINAL') {
-      originalScale = false;
-      toggleScale();
+      Lib.getConfig().originalScale = false;
+      Lib.toggleScale();
     } else if (data.action === 'SCALE_WINDOW') {
-      originalScale = true;
-      toggleScale();
+      Lib.getConfig().originalScale = true;
+      Lib.toggleScale();
     } else if (data.action === 'MUTE_AUDIO') {
-      soundEnabled = true;
-      toggleSound();
+      Lib.getConfig().soundEnabled = true;
+      Lib.toggleSound();
     } else if (data.action === 'UNMUTE_AUDIO') {
-      soundEnabled = false;
-      toggleSound();
+      Lib.getConfig().soundEnabled = false;
+      Lib.toggleSound();
     } else if (data.action === 'BUTTON_DOWN') {
-      handleButtonDown(data.payload);
+      Lib.handleButtonDown(data.payload);
     } else if (data.action === 'BUTTON_UP') {
-      handleButtonUp(data.payload);
+      Lib.handleButtonUp(data.payload);
     } else if (data.action === 'BEGIN_GAME') {
       if (window.start) {
         window.start();
@@ -192,29 +305,41 @@ window.addEventListener('message', event => {
 });
 // required for wasm to grab keyboard controls
 setInterval(() => {
-  window.focus();
+  if (Lib.getConfig().gameStarted) {
+    window.focus();
+  }
 }, 500);
 
-function localInit() {
+async function localInit() {
   window.loadTimeout = setTimeout(function () {
     console.error('[IFRAME] Content took too long to load.');
-    showError();
+    Lib.showError();
   }, 30000);
-  window.init();
+  await window.init();
 }
+
+function verify() {
+  if (!window.init) {
+    console.error('[IFRAME] no `init` found.');
+  }
+  if (!window.start) {
+    console.error('[IFRAME] no `start` found.');
+  }
+}
+verify();
 
 var queryString = window.location.search;
 var params = new URLSearchParams(queryString);
 var expand = params.get('cabinet');
 if (expand === 'true') {
-  shouldShowControls = true;
+  Lib.getConfig().shouldShowControls = true;
   const startButton = document.getElementById('start');
-  startButtonEnabled = false;
+  Lib.getConfig().startButtonEnabled = false;
   if (startButton) startButton.style.display = 'none';
   window.addEventListener('load', () => {
-    console.log('loaded iframe lib');
-    toggleControls();
-    toggleScale();
+    console.log('[IFRAME] loaded Lib');
+    Lib.toggleControls();
+    Lib.toggleScale();
     var toggleScaleElem = document.getElementById('toggle-scale');
     if (toggleScaleElem) toggleScaleElem.style.display = 'none';
     var toggleSoundElem = document.getElementById('toggle-sound');
@@ -225,13 +350,13 @@ if (expand === 'true') {
 var tapToStart = params.get('tap');
 if (tapToStart === 'true') {
   var div = document.createElement('div');
-  var loading = document.getElementById('loading');
-  if (loading) {
-    loading.style.display = 'none';
+  var loadingElem = document.getElementById('loading');
+  if (loadingElem) {
+    loadingElem.style.display = 'none';
   }
   window.onTapToStart = function () {
-    if (loading) {
-      loading.style.display = 'flex';
+    if (loadingElem) {
+      loadingElem.style.display = 'flex';
     }
     div.style.display = 'none';
     localInit();
